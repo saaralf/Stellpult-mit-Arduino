b67a1fb7e2db4b8e340ab751c25326d564d09de9
Michael Keller <keller-michael@web.de>
2021-11-23 21:09:02 +0100

    mqtt zugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,23 @@
-#include <Arduino.h>
 #include <Adafruit_MCP23X17.h>
+#include <Arduino.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+#include <SPI.h>
+
+// Update these with values suitable for your network.
+byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED};
+IPAddress ip(192, 168, 178, 150);
+IPAddress mqtt_server(192, 168, 178, 8);
+String clientId = "Stellpult";
+
+#define outTopic "Stellpult/outtopic"
+#define inTopic "Stellpult/intopic"
+#define mqtt_user "MQTT_username"
+#define mqtt_password "MQTT_PW"
+#define mqttHostname "Stellpult_MQTT"
+
+EthernetClient ethClient;
+PubSubClient client(ethClient);
 
 #define MAXLEDMCP 5
 #define MAXGPIO 16
@@ -88,14 +106,14 @@
 #define SIGNALGRUEN7 2 // Signal 7 Gruen MCP 2 GPIOA2
 
 //MCP 2 GPIOA 3-7 -> LED 22 - 25
-#define LED21 3 // MCP 2 GPIOA3
-#define LED22 4 // MCP 2 GPIOA4
-#define LED23 5 // MCP 2 GPIOA5
-#define LED24 6 // MCP 2 GPIOA6
-#define LED25 7 // MCP 2 GPIOA7
+#define LED22 3 // MCP 2 GPIOA3
+#define LED23 4 // MCP 2 GPIOA4
+#define LED24 5 // MCP 2 GPIOA5
+#define LED25 6 // MCP 2 GPIOA6
+#define LED26 7 // MCP 2 GPIOA7
 //MCP 2 GPIOB 0 - 7 -> LED 25 - LED XX
-#define LED26 8 // MCP 2 GPIOB0
-#define LED28 9 // MCP 2 GPIOB1
+#define LED27 8  // MCP 2 GPIOB0
+#define LED28 9  // MCP 2 GPIOB1
 #define LED29 10 // MCP 2 GPIOB2
 #define LED30 11 // MCP 2 GPIOB3
 #define LED31 12 // MCP 2 GPIOB4
@@ -103,34 +121,96 @@
 #define LED33 14 // MCP 2 GPIOB6
 #define LED34 15 // MCP 2 GPIOB7
 
-#define SIGNALROT8 0   // Signal 8 Rot   MCP 3 GPIOA0
-#define SIGNALGRUEN8 1 // Signal 8 Gruen MCP 3 GPIOA1
-#define SIGNALROT9 2   // Signal 9 Rot   MCP 3 GPIOA2
-#define SIGNALGRUEN9 3 // Signal 9 Gruen MCP 3 GPIOA3
-#define SIGNALROT10 4   // Signal 10 Rot   MCP 3 GPIOA4
-#define SIGNALGRUEN10 5 // Signal 10 Gruen MCP 3 GPIOA5
-#define SIGNALROT11 6   // Signal 11 Rot   MCP 3 GPIOA6
-#define SIGNALGRUEN11 7 // Signal 11 Gruen MCP 3 GPIOA7
-#define SIGNALROT12 8 // Signal 12 Rot   MCP 3 GPIOB0
-#define SIGNALGRUEN12 9 // Signal 12 Gruen MCP 3 GPIOB1
+#define SIGNALROT8 0     // Signal 8 Rot   MCP 3 GPIOA0
+#define SIGNALGRUEN8 1   // Signal 8 Gruen MCP 3 GPIOA1
+#define SIGNALROT9 2     // Signal 9 Rot   MCP 3 GPIOA2
+#define SIGNALGRUEN9 3   // Signal 9 Gruen MCP 3 GPIOA3
+#define SIGNALROT10 4    // Signal 10 Rot   MCP 3 GPIOA4
+#define SIGNALGRUEN10 5  // Signal 10 Gruen MCP 3 GPIOA5
+#define SIGNALROT11 6    // Signal 11 Rot   MCP 3 GPIOA6
+#define SIGNALGRUEN11 7  // Signal 11 Gruen MCP 3 GPIOA7
+#define SIGNALROT12 8    // Signal 12 Rot   MCP 3 GPIOB0
+#define SIGNALGRUEN12 9  // Signal 12 Gruen MCP 3 GPIOB1
 #define SIGNALROT13 10   // Signal 13 Rot   MCP 3 GPIOB2
 #define SIGNALGRUEN13 11 // Signal 13 Gruen MCP 3 GPIOB3
 #define SIGNALROT14 12   // Signal 14 Rot   MCP 3 GPIOB4
 #define SIGNALGRUEN14 13 // Signal 14 Gruen MCP 3 GPIOB5
 #define SIGNALROT15 14   // Signal 15 Rot   MCP 3 GPIOB6
 #define SIGNALGRUEN15 15 // Signal 15 Gruen MCP 3 GPIOB7
-#define SIGNALROT16 0   // Signal 16 Rot   MCP 4 GPIOA0
-#define SIGNALGRUEN16 0 // Signal 16 Gruen MCP 4 GPIOA1
+#define SIGNALROT16 0    // Signal 16 Rot   MCP 4 GPIOA0
+#define SIGNALGRUEN16 0  // Signal 16 Gruen MCP 4 GPIOA1
 
 //Taster
 //MCP 4
 
 Adafruit_MCP23X17 mcp[MAXLEDMCP];
 
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  for (unsigned int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  if (String(topic) == inTopic)
+  {
+    if (messageTemp == "ON")
+    {
+      Serial.print("Changing LED to ON\n");
+      //  digitalWrite(LEDPin, HIGH);
+      //Invertiertes Signal
+      client.publish(outTopic, "ON");
+      delay(200);
+    }
+    else if (messageTemp == "OFF")
+    {
+      Serial.print("Changing LED to OFF\n");
+      //digitalWrite(LEDPin, LOW); //Invertiertes Signal
+      client.publish(outTopic, "OFF");
+      delay(200);
+    }
+  }
+}
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_user, mqtt_password))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(outTopic, mqttHostname);
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
   //MCP fÃ¼r LEDs
   mcp[0].begin_I2C(MCP1ADDR); // FÃ¼r LEDs 1 - 16
   mcp[1].begin_I2C(MCP2ADDR); // FÃ¼r LEDs 17 - 21 und Signal 1 - 6
@@ -150,7 +230,7 @@ void setup()
     mcp[2].pinMode(gpio, OUTPUT);
     mcp[3].pinMode(gpio, OUTPUT);
   }
-    for (int gpio = 0; gpio < MAXGPIO; gpio++)
+  for (int gpio = 0; gpio < MAXGPIO; gpio++)
   {
     mcp[4].pinMode(gpio, INPUT_PULLUP);
     mcp[5].pinMode(gpio, INPUT_PULLUP);
@@ -177,9 +257,7 @@ void setup()
   mcp[1].digitalWrite(SIGNALGRUEN5, LOW);
   mcp[2].digitalWrite(SIGNALGRUEN6, LOW);
   mcp[2].digitalWrite(SIGNALGRUEN7, LOW);
-
 }
-
 
 void loop()
 {
